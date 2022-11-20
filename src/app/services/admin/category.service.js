848c31be00c304ade2f3597a167c14adab81dda0
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-11-20 13:53:46 +0700

    Update category.service.js

--- src/app/services/admin/category.service.js
+++ src/app/services/admin/category.service.js
@@ -1,8 +1,8 @@
-import api from '../../axios/api';
+import api from "../../axios/api";
 
 export const getCategory = async () => {
   try {
-    const response = await api.get('category/getAllCategories');
+    const response = await api.get("category/getAllCategories");
     if (response.status === 200) {
       return response.data;
     }
@@ -13,7 +13,7 @@ export const getCategory = async () => {
 
 export const getOneCategory = async (id) => {
   try {
-    const response = await api.get('category/getOneCategory/' + id);
+    const response = await api.get("category/getOneCategory/" + id);
     if (response.status === 200) {
       return response.data;
     }
@@ -23,7 +23,7 @@ export const getOneCategory = async (id) => {
 };
 export const getParentCategory = async () => {
   try {
-    const response = await api.get('category/getCategoriesParent');
+    const response = await api.get("category/getCategoriesParent");
     if (response.status === 200) {
       return response.data;
     }
@@ -33,8 +33,8 @@ export const getParentCategory = async () => {
 };
 export const UpdateCategory = async (data) => {
   try {
-    const response = await api.put('category', data);
-    if (response.status === 200) {
+    const response = await api.put("category", data);
+    if (response.status === 201 || response.status === 200) {
       return response.data;
     }
   } catch (err) {
@@ -43,11 +43,21 @@ export const UpdateCategory = async (data) => {
 };
 export const DeleteCategory = async (id) => {
   try {
-    const response = await api.delete('category?id=' + id);
+    const response = await api.delete("category?id=" + id);
     if (response.status === 200) {
       return response.data;
     }
   } catch (err) {
     throw new Error(err);
   }
-}
+};
+export const AddCategory = async (data) => {
+  try {
+    const response = await api.post("category", data);
+    if (response.status === 201 || response.status === 200) {
+      return response;
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
