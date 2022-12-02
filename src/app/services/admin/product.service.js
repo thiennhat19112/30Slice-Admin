8ac5349b853c967207d313263bdb2b660d59a9dc
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2022-12-02 09:51:32 +0700

    service product

--- src/app/services/admin/product.service.js
+++ src/app/services/admin/product.service.js
@@ -12,3 +12,42 @@ export const getProducts = async () => {
     }
     
 };
+
+export const getOneProduct = async (id) => {
+    try {
+      const response = await api.get("product/getOneProduct/" + id);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+  
+  export const addProduct = async (data) => {
+    try {
+      const res = await api.post("product/", data);
+      return res;
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+  
+  export const updateProduct = async (data) => {
+    try {
+      const res = await api.put("product/", data);
+      return res;
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+  
+  export const deleteProduct = async (data) => {s
+    try {
+      const res = await api.delete("product/", { data });
+      return res;
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,6 +1,29 @@
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
 import { Eye, Edit, XCircle } from 'react-feather';
+import { getProducts } from '../../app/services/admin/product.service';
 
 const Product = () => {
+  const _isMounted = useRef(false)
+  const [products,setProducts] = useState([])
+
+  const fetchProduct = async () => {
+    const data = await getProducts()
+    return setProducts(data);
+  }
+
+  useEffect(()=>{
+    _isMounted.current = true
+    return ()=> {
+      _isMounted.current = false
+    }
+  })
+
+  useEffect(()=>{
+    fetchProduct()
+  },[])
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -9,9 +32,9 @@ const Product = () => {
             <div className=" d-flex flex-wrap justify-content-center breadcrumb-main__wrapper">
               <div className="d-flex align-items-center user-member__title justify-content-center mr-sm-25">
                 <h4 className="text-capitalize fw-500 breadcrumb-title">
-                  users list datatable
+                  Quản lí sản phẩm
                 </h4>
-                <span className="sub-title ml-sm-25 pl-sm-25">274 Users</span>
+                <span className="sub-title ml-sm-25 pl-sm-25">{products?.length} sản phẩm</span>
               </div>
               <form
                 action="/"
@@ -34,221 +57,8 @@ const Product = () => {
                 data-target="#new-member"
               >
                 <i className="las la-plus fs-16" />
-                Add New Member
+               Thêm sản phẩm
               </a>
-              {/* Modal */}
-              <div
-                className="modal fade new-member"
-                id="new-member"
-                role="dialog"
-                tabIndex={-1}
-                aria-labelledby="staticBackdropLabel"
-                aria-hidden="true"
-              >
-                <div className="modal-dialog modal-dialog-centered">
-                  <div className="modal-content  radius-xl">
-                    <div className="modal-header">
-                      <h6
-                        className="modal-title fw-500"
-                        id="staticBackdropLabel"
-                      >
-                        Create project
-                      </h6>
-                      <button
-                        type="button"
-                        className="close"
-                        data-dismiss="modal"
-                        aria-label="Close"
-                      >
-                        <span data-feather="x" />
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      <div className="new-member-modal">
-                        <form>
-                          <div className="form-group mb-20">
-                            <input
-                              type="text"
-                              className="form-control"
-                              placeholder="Duran Clayton"
-                            />
-                          </div>
-                          <div className="form-group mb-20">
-                            <div className="category-member">
-                              <select
-                                className="js-example-basic-single js-states form-control"
-                                id="category-member"
-                              >
-                                <option value="JAN">1</option>
-                                <option value="FBR">2</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="form-group mb-20">
-                            <textarea
-                              className="form-control"
-                              id="exampleFormControlTextarea1"
-                              rows={3}
-                              placeholder="Project description"
-                              defaultValue={''}
-                            />
-                          </div>
-                          <div className="form-group textarea-group">
-                            <label className="mb-15">status</label>
-                            <div className="d-flex">
-                              <div className="project-task-list__left d-flex align-items-center">
-                                <div className="checkbox-group d-flex mr-50 pr-10">
-                                  <div className="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                    <input
-                                      className="checkbox"
-                                      type="checkbox"
-                                      id="check-grp-1"
-                                      defaultChecked=""
-                                    />
-                                    <label
-                                      htmlFor="check-grp-1"
-                                      className="fs-14 color-light strikethrough"
-                                    >
-                                      status
-                                    </label>
-                                  </div>
-                                </div>
-                                <div className="checkbox-group d-flex mr-50 pr-10">
-                                  <div className="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                    <input
-                                      className="checkbox"
-                                      type="checkbox"
-                                      id="check-grp-2"
-                                    />
-                                    <label
-                                      htmlFor="check-grp-2"
-                                      className="fs-14 color-light strikethrough"
-                                    >
-                                      Deactivated
-                                    </label>
-                                  </div>
-                                </div>
-                                <div className="checkbox-group d-flex">
-                                  <div className="checkbox-theme-default custom-checkbox checkbox-group__single d-flex">
-                                    <input
-                                      className="checkbox"
-                                      type="checkbox"
-                                      id="check-grp-3"
-                                    />
-                                    <label
-                                      htmlFor="check-grp-3"
-                                      className="fs-14 color-light strikethrough"
-                                    >
-                                      bloked
-                                    </label>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="mb-25">
-                            <div className="form-group mb-10">
-                              <label htmlFor="name47">project member</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                id="name47"
-                                placeholder="Search members"
-                              />
-                            </div>
-                            <ul className="d-flex flex-wrap mb-20 user-group-people__parent">
-                              <li>
-                                <a href="#">
-                                  <img
-                                    className="rounded-circle wh-34"
-                                    src="img/tm1.png"
-                                    alt="author"
-                                  />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <img
-                                    className="rounded-circle wh-34"
-                                    src="img/tm2.png"
-                                    alt="author"
-                                  />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <img
-                                    className="rounded-circle wh-34"
-                                    src="img/tm3.png"
-                                    alt="author"
-                                  />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <img
-                                    className="rounded-circle wh-34"
-                                    src="img/tm4.png"
-                                    alt="author"
-                                  />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="#">
-                                  <img
-                                    className="rounded-circle wh-34"
-                                    src="img/tm5.png"
-                                    alt="author"
-                                  />
-                                </a>
-                              </li>
-                            </ul>
-                          </div>
-                          <div className="d-flex new-member-calendar">
-                            <div className="form-group w-100 mr-sm-15 form-group-calender">
-                              <label htmlFor="datepicker">start Date</label>
-                              <div className="position-relative">
-                                <input
-                                  type="text"
-                                  className="form-control"
-                                  id="datepicker"
-                                  placeholder="mm/dd/yyyy"
-                                />
-                                <a href="#">
-                                  <span data-feather="calendar" />
-                                </a>
-                              </div>
-                            </div>
-                            <div className="form-group w-100 form-group-calender">
-                              <label htmlFor="datepicker2">End Date</label>
-                              <div className="position-relative">
-                                <input
-                                  type="text"
-                                  className="form-control"
-                                  id="datepicker2"
-                                  placeholder="mm/dd/yyyy"
-                                />
-                                <a href="#">
-                                  <span data-feather="calendar" />
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="button-group d-flex pt-25">
-                            <button className="btn btn-primary btn-default btn-squared text-capitalize">
-                              add new project
-                            </button>
-                            <button className="btn btn-light btn-default btn-squared fw-400 text-capitalize b-light color-light">
-                              cancel
-                            </button>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Modal */}
             </div>
           </div>
         </div>
@@ -261,32 +71,19 @@ const Product = () => {
                 <thead>
                   <tr className="userDatatable-header">
                     <th>
-                      <div className="d-flex align-items-center">
-                        <div className="custom-checkbox  check-all">
-                          <input
-                            className="checkbox"
-                            type="checkbox"
-                            id="check-3"
-                          />
-                          <label htmlFor="check-3">
-                            <span className="checkbox-text userDatatable-title">
-                              user
-                            </span>
-                          </label>
-                        </div>
-                      </div>
+                    <span className="userDatatable-title">Tên sản phẩm</span>
                     </th>
                     <th>
-                      <span className="userDatatable-title">emaill</span>
+                      <span className="userDatatable-title">Hình sản phẩm</span>
                     </th>
                     <th>
-                      <span className="userDatatable-title">company</span>
+                      <span className="userDatatable-title">Loại</span>
                     </th>
                     <th>
-                      <span className="userDatatable-title">position</span>
+                      <span className="userDatatable-title">Ẩn/hiện</span>
                     </th>
                     <th>
-                      <span className="userDatatable-title">join date</span>
+                      <span className="userDatatable-title">Nỗi bật</span>
                     </th>
                     <th>
                       <span className="userDatatable-title">status</span>
