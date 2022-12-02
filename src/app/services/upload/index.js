a69501fb788db7899b015583590f6d53ffce605f
thiennhat19112 <92378019+thiennhat19112@users.noreply.github.com>
2022-12-02 12:34:11 +0700

    update upload

--- src/app/services/upload/index.js
+++ src/app/services/upload/index.js
@@ -7,28 +7,16 @@ import {
 import { toastError } from "../../../components/sharedComponents/toast";
 
 export async function uploadLoadFIle(file) {
-  
+  const filename = new Date().getTime() + file.name
   const storage = getStorage();
   if (!file) {
     toastError("Vui lòng chọn hình ảnh!");
     return
   }
   
-  const storageRef = ref(storage, `/img/${file.name}`);
-  const uploadTask = uploadBytesResumable(storageRef, file);
-   uploadTask.on(
-    "state_changed",
-    (snapshot) => {
-      const percent = Math.round(
-        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      );
-
-      console.log(percent);
-    },
-    (err) => console.log(err),
-    
-  );
-  const url = await getDownloadURL(uploadTask.snapshot.ref);
+  const storageRef = ref(storage, `/img/${filename}`);
+  await uploadBytesResumable(storageRef, file);
+  const url = await getDownloadURL(storageRef);
   return url
 
 }
